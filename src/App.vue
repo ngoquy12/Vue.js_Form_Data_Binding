b58f26daa1407894930ec073b3f7b0ae976138b3
ngovanquy <ngoq753429@gmail.com>
2024-10-01 15:20:24 +0700

    upadate files

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- <OnewayDataBinding /> -->
+  <TwowayDataBinding />
+</template>
+
+<script setup>
+import OnewayDataBinding from "./components/OnewayDataBinding.vue";
+import TwowayDataBinding from "./components/TwowayDataBinding.vue";
+</script>
+
+<style></style>
---
+++ src/components/OnewayDataBinding.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Oneway data binding</h1>
+    <h3>Username: {{ userName }}</h3>
+    <input type="text" :value="userName" />
+    <button @click="handleChangeName">Change Name</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const userName = ref("Nguyễn Văn B");
+
+const handleChangeName = () => {
+  userName.value = "Nguyễn Văn A";
+};
+</script>
+
+<style></style>
